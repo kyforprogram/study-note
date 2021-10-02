d3dfc35bdcffe7ad49e18bfbdf32289e62fcfb31
kyforprogram <kyforprogram@gmail.com>
2021-10-02 07:00:51 +0000

    初級jQueryの基礎.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'jqueries/index'
+  root 'jqueries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
